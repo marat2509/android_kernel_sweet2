82dd373bfd681d0d2eb9e23a85bf3a50a4d8d987
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-15 05:32:24 +0700

    msm: drm: Don't use global variable for drm notifier data
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -56,8 +56,6 @@ static struct wakeup_source prim_panel_wakelock;
 extern void dsi_display_panel_gamma_mode_change(struct dsi_display *display,
 			struct dsi_display_mode *adj_mode);
 
-struct msm_drm_notifier g_notify_data;
-
 static void convert_to_dsi_mode(const struct drm_display_mode *drm_mode,
 				struct dsi_display_mode *dsi_mode)
 {
@@ -204,7 +202,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 
 	event = dev->doze_state;
 
-	g_notify_data.data = &event;
+	notify_data.data = &event;
 
 	if (!bridge) {
 		pr_err("Invalid params\n");
@@ -245,7 +243,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		}
 	}
 
-	msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &g_notify_data);
+	msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
 
 	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
 
@@ -274,7 +272,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		(void)dsi_display_unprepare(c_bridge->display);
 	}
 
-	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &g_notify_data);
+	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 
 	SDE_ATRACE_END("dsi_display_enable");
 
@@ -516,6 +514,7 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 	int rc = 0;
 	struct dsi_bridge *c_bridge = to_dsi_bridge(bridge);
 	struct drm_device *dev = bridge->dev;
+	struct msm_drm_notifier notify_data;
 	int event = 0;
 	int power_mode;
 
@@ -525,8 +524,7 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 	}
 
 	event = dev->doze_state;
-
-	g_notify_data.data = &event;
+	notify_data.data = &event;
 
 	if (!bridge) {
 		pr_err("Invalid params\n");
@@ -541,6 +539,7 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 
 	SDE_ATRACE_BEGIN("dsi_bridge_post_disable");
 	SDE_ATRACE_BEGIN("dsi_display_disable");
+
 	rc = dsi_display_disable(c_bridge->display);
 	if (rc) {
 		pr_err("[%d] DSI display disable failed, rc=%d\n",
@@ -559,7 +558,7 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 	}
 	SDE_ATRACE_END("dsi_bridge_post_disable");
 
-	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &g_notify_data);
+	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 
 	if (c_bridge->display->is_prim_display)
 		atomic_set(&prim_panel_is_on, false);
